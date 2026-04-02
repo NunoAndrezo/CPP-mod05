f501bc0b754b5bc3a61b48a2aa44eb7747a21161
nuno <nunoandrezo97@gmail.com>
2026-04-02 13:58:14 +0100

    Added a way to remove *_shrubbery files via Makefile

---
+++ ex02/src/PresidentialPardonForm.cpp
@@ -0,0 +1,3 @@
+
+/* PresidentialPardonForm: Required grades: sign 25, exec 5
+Informs that <target> has been pardoned by Zaphod Beeblebrox */
--- ex02/src/ShrubberyCreationForm.cpp
+++ ex02/src/ShrubberyCreationForm.cpp
@@ -37,4 +37,55 @@ const std::string ShrubberyCreationForm::getTarget() const
 	return (this->_target);
 }
 
-
+/* ShrubberyCreationForm: Required grades: sign 145, exec 137
+Creates a file <target>_shrubbery in the working directory and writes ASCII trees
+inside it. */
+void ShrubberyCreationForm::execute(Bureaucrat const &executor) const
+{
+	if (!getIsSigned())
+		throw AForm::GradeTooLowException(); // if the form is not signed, we can't execute it, so we throw an exception.
+	else if (executor.getGrade() > getGrade_Execute())
+		throw AForm::GradeTooLowException(); // if the executor's grade is too low to execute the form, we throw an exception.
+	std::ofstream file((getTarget() + "_shrubbery").c_str()); // create a file with the name of the target + "_shrubbery"
+	if (!file.is_open())
+	{
+		std::cerr << "Error: Could not open file " << getTarget() + "_shrubbery" << std::endl;
+		return;
+	}
+	else
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			file << "                                                    ." << std::endl;
+			file << "                                              .         ;  " << std::endl;
+			file << "                 .              .              ;%     ;;    " << std::endl;
+			file << "                   ,           ,                :;%  %;   " << std::endl;
+			file << "                    :         ;                   :;%;'     .,   " << std::endl;
+			file << "           ,.        %;     %;            ;        %;'    ,;" << std::endl;
+			file << "             ;       ;%;  %%;        ,     %;    ;%;    ,%'" << std::endl;
+			file << "              %;       %;%;      ,  ;       %;  ;%;   ,%;' "<< std::endl;
+			file << "               ;%;      %;        ;%;        % ;%;  ,%;'" << std::endl;
+			file << "                `%;.     ;%;     %;'         `;%%;.%;'" << std::endl;
+			file << "                 `:;%.    ;%%. %@;        %; ;@%;%'" << std::endl;
+			file << "                    `:%;.  :;bd%;          %;@%;'   "<< std::endl;
+			file << "                      `@%:.  :;%.         ;@@%;'    "<< std::endl;  
+			file << "                        `@%.  `;@%.      ;@@%;      "<< std::endl;      
+			file << "                          `@%%. `@%%    ;@@%;       "<< std::endl;    
+			file << "                            ;@%. :@%%  %@@%;        "<< std::endl;  
+			file << "                              %@bd%%%bd%%:;         "<< std::endl;
+			file << "                                #@%%%%%:;;          "<< std::endl;
+			file << "                                %@@%%%::;           "<< std::endl;
+			file << "                                %@@@%(o);  . '      "<< std::endl;      
+			file << "                                %@@@o%;:(.,'        "<< std::endl;    
+			file << "                            `.. %@@@o%::;           "<< std::endl; 
+			file << "                               `)@@@o%::;           "<< std::endl; 
+			file << "                                %@@(o)::;           "<< std::endl;
+			file << "                               .%@@@@%::;           "<< std::endl; 
+			file << "                               ;%@@@@%::;.          "<< std::endl;   
+			file << "                              ;%@@@@%%:;;;.         "<< std::endl;
+			file << "                          ...;%@@@@@%%:;;;;,..      " << std::endl;
+			file << std::endl;
+		}
+		std::cout << "Shrubbery created in file: " << getTarget() + "_shrubbery" << std::endl;
+	}
+}
